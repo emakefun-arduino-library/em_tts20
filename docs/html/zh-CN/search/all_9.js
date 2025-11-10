e745d8831da256fb2e78b20736b185d17e03a21b
“xianlonluohan” <2047669296@qq.com>
2025-11-10 11:24:09 +0800

    tts20模块的Arduino库初次提交

---
+++ docs/html/zh-CN/search/all_9.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['tts20_0',['Tts20',['../classem_1_1_tts20.html',1,'em::Tts20'],['../classem_1_1_tts20.html#aea3ee289727d68dc8998b206f07b361d',1,'em::Tts20::Tts20()']]],
+  ['tts20_2ecpp_1',['tts20.cpp',['../tts20_8cpp.html',1,'']]],
+  ['tts20_2eh_2',['tts20.h',['../tts20_8h.html',1,'']]],
+  ['tts20_5flib_2eh_3',['tts20_lib.h',['../tts20__lib_8h.html',1,'']]]
+];
---
+++ docs/html/zh-CN/search/functions_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['pause_0',['Pause',['../classem_1_1_tts20.html#a0caa9585487c9df905063b4dfd648b7b',1,'em::Tts20']]],
+  ['play_1',['Play',['../classem_1_1_tts20.html#a67060bb832abb5b04e51832ed26ebab8',1,'em::Tts20']]]
+];
